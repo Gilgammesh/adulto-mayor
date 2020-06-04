031996cebd9a23cc7e37c63469b77ad785085182
Carlos Santander <carlos.santander@gmail.com>
2020-06-04 18:31:52 -0500

    update order

--- frontend/src/components/Admin/Tabla/Index.js
+++ frontend/src/components/Admin/Tabla/Index.js
@@ -14,6 +14,10 @@ const Index = () => {
     variables: {
       sortby: [
         {
+          field: "createdAt",
+          direction: "DESC",
+        },
+        {
           field: "provincia",
           direction: "ASC",
         },
